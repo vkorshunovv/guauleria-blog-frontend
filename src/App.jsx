c10f4634e740210365486654ac6987d470e0d964
Viktor Korshunov <viktorkorshunov@MacBook-Air-Viktor.local>
2024-06-22 10:43:00 +0200

    add fetching and rendering posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [post, setPosts] = useState([]);
+  const [newPost, setNewPost] = useState({ title: "", content: "" });
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const fetchPosts = () => {
+    axios
+      .get("http://localhost:5000/api/posts")
+      .then((response) => {
+        setPosts(response.data);
+      })
+      .catch((error) => {
+        console.log("Error fetching data:", error);
+      });
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setNewPost((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:5000/api/posts", newPost)
+      .then((response) => {
+        console.log("Post added:", response.data);
+        setNewPost({ title: "", content: "" });
+        fetchPosts();
+      })
+      .catch((error) => {
+        console.log("Error adding post:", error);
+      });
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Blogs</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title"> Title:</label>
+        <input
+          id="title"
+          type="text"
+          name="title"
+          value={newPost.title}
+          onChange={handleInputChange}
+          required
+        />
+        <br />
+        <label htmlFor="content">Content:</label>
+        <textarea
+          id="content"
+          name="content"
+          value={newPost.content}
+          onChange={handleInputChange}
+          required
+        />
+        <br />
+        <button type="submit">Add Post</button>
+      </form>
+      <ul>
+        {posts.map((post) => {
+          <li key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.content}</p>
+          </li>;
+        })}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
